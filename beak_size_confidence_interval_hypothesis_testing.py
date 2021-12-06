d688bb2514f19012068d7db43b999a35bace4d0a
Nachiket Paranjape <nachiketm.paranjape@gmail.com>
2021-12-06 11:30:22 -0800

    Update and rename confidence_interval_hypothesis_testing.py to beak_size_confidence_interval_hypothesis_testing.py

--- beak_size_confidence_interval_hypothesis_testing.py
+++ beak_size_confidence_interval_hypothesis_testing.py
@@ -1,3 +1,20 @@
+def bootstrap_replicate_1d(data, func):
+    """Generate bootstrap replicate of 1D data."""
+    bs_sample = np.random.choice(data, len(data))
+    return func(bs_sample)
+  
+def draw_bs_reps(data, func, size=1):
+    """Draw bootstrap replicates."""
+
+    # Initialize array of replicates: bs_replicates
+    bs_replicates = np.empty(size=size)
+
+    # Generate replicates
+    for i in range(size):
+        bs_replicates[i] = bootstrap_replicate_1d(data,func)
+
+    return bs_replicates
+
 # confidence interval for the difference
 
 # Compute the difference of the sample means: mean_diff
