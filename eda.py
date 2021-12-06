96d70ff21543a59792ba8dc33960fc633b966a5a
Nachiket Paranjape <nachiketm.paranjape@gmail.com>
2021-12-06 11:28:56 -0800

    Update eda.py

--- eda.py
+++ eda.py
@@ -64,56 +64,5 @@ _ = plt.legend(('1975', '2012'), loc='lower right')
 plt.show()
 
 
-# confidence interval for the difference
-
-# Compute the difference of the sample means: mean_diff
-mean_diff = np.mean(bd_2012) - np.mean(bd_1975)
-
-# Get bootstrap replicates of means
-bs_replicates_1975 = draw_bs_reps(bd_1975, np.mean, 10000)
-bs_replicates_2012 = draw_bs_reps(bd_2012, np.mean, 10000)
-
-# Compute samples of difference of means: bs_diff_replicates
-bs_diff_replicates = bs_replicates_2012 - bs_replicates_1975
-
-# Compute 95% confidence interval: conf_int
-conf_int = np.percentile(bs_diff_replicates, [2.5, 97.5])
-
-# Print the results
-print('difference of means =', mean_diff, 'mm')
-print('95% confidence interval =', conf_int, 'mm')
-
-# output
-# difference of means = 0.22622047244094645 mm
-# 95% confidence interval = [0.05633521 0.39190544] mm
-
-## Hypothesis test: Are beaks deeper in 2012?
-# null hypothesis : mean of the two datasets is the same
-# we simulate data to assume their mean equal and calculate p-value for observed difference in mean
-
-# Compute mean of combined data set: combined_mean
-combined_mean = np.mean(np.concatenate((bd_1975, bd_2012)))
-
-# Shift the samples
-bd_1975_shifted = bd_1975 - np.mean(bd_1975) + combined_mean
-bd_2012_shifted = bd_2012 - np.mean(bd_2012) + combined_mean
-
-# Get bootstrap replicates of shifted data sets
-bs_replicates_1975 = draw_bs_reps(bd_1975_shifted,np.mean,10000)
-bs_replicates_2012 = draw_bs_reps(bd_2012_shifted,np.mean,10000)
-
-# Compute replicates of difference of means: bs_diff_replicates
-bs_diff_replicates = bs_replicates_2012 - bs_replicates_1975
-
-# Compute the p-value
-p = np.sum(bs_diff_replicates >= mean_diff) / len(bs_diff_replicates)
-
-# Print p-value
-print('p =', p)
-
-## output
-# p = 0.0034
-
-
 
 
